411aff02d331a85dc7071373abd770a51d7b6e8f
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-05-30 15:07:55 -0500

    Add print_date, print_products_header, print_brands_header, and start methods to app.rb in toycity2 project.

--- toycity2/lib/app.rb
+++ toycity2/lib/app.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # Get path to products.json, read the file into a string,
 # and transform the string into a usable hash
 def setup_files
@@ -8,7 +10,7 @@ def setup_files
 end
 
 # Print "Sales Report" in ascii art
-def print_sales_report
+def print_sales_header
   puts " _____       _            ______                      _"
   puts "/  ___|     | |           | ___ \\                    | |"
   puts "\\ `--.  __ _| | ___  ___  | |_/ /___ _ __   ___  _ __| |_"
@@ -20,8 +22,24 @@ def print_sales_report
 end
 
 # Print today's date
+def print_date
+  date = Time.new
+  puts "Today's Date: #{date.month}/#{date.day}/#{date.year}"
+  puts
+end
 
 # Print "Products" in ascii art
+def print_products_header
+  puts "                     _            _       "
+  puts "                    | |          | |      "
+  puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+  puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+  puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+  puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+  puts "| |                                       "
+  puts "|_|                                       "
+  puts
+end
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -32,6 +50,15 @@ end
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
+def print_brands_header
+  puts " _                         _     "
+  puts "| |                       | |    "
+  puts "| |__  _ __ __ _ _ __   __| |___ "
+  puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+  puts "| |_) | | | (_| | | | | (_| \\__ \\"
+  puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+  puts
+end
 
 # For each brand in the data set:
 	# Print the name of the brand
@@ -39,4 +66,12 @@ end
 	# Calculate and print the average price of the brand's toys
 	# Calculate and print the total sales volume of all the brand's toys combined
 
-print_sales_report
+def start
+  setup_files
+  print_sales_header
+  print_date
+  print_products_header
+  print_brands_header
+end
+	
+start
