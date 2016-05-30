7d2f3682fbf6d5ced6ab3003d3f42fdcd15c2284
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-05-30 14:54:25 -0500

    Add print_sales_report method to toycity2.

--- toycity2/lib/app.rb
+++ toycity2/lib/app.rb
@@ -1,13 +1,23 @@
 # Get path to products.json, read the file into a string,
 # and transform the string into a usable hash
 def setup_files
-    path = File.join(File.dirname(__FILE__), '../data/products.json')
-    file = File.read(path)
-    $products_hash = JSON.parse(file)
-    $report_file = File.new("report.txt", "w+")
+  path = File.join(File.dirname(__FILE__), '../data/products.json')
+  file = File.read(path)
+  $products_hash = JSON.parse(file)
+  $report_file = File.new("report.txt", "w+")
 end
 
 # Print "Sales Report" in ascii art
+def print_sales_report
+  puts " _____       _            ______                      _"
+  puts "/  ___|     | |           | ___ \\                    | |"
+  puts "\\ `--.  __ _| | ___  ___  | |_/ /___ _ __   ___  _ __| |_"
+  puts " `--. \\/ _` | |/ _ \\/ __| |    // _ \\ '_ \\ / _ \\| '__| __|"
+  puts "/\\__/ / (_| | |  __/\\__ \\ | |\\ \\  __/ |_) | (_) | |  | |_"
+  puts "\\____/ \\__,_|_|\\___||___/ \\_| \\_\\___| .__/ \\___/|_|   \\__|"
+  puts "====================================| |==================="
+  puts "                                    |_|"
+end
 
 # Print today's date
 
@@ -28,3 +38,5 @@ end
 	# Count and print the number of the brand's toys we stock
 	# Calculate and print the average price of the brand's toys
 	# Calculate and print the total sales volume of all the brand's toys combined
+
+print_sales_report
