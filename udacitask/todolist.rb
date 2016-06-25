c330082e8fe1d0ee17168c78591934823711bc5a
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-06-25 15:52:01 -0500

    Implement due_date display feature for Item. Add column headers to ToDoList

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -3,13 +3,13 @@ class ToDoList
   
   # Initialize todo list with a title and no items
   def initialize(list_title)
-    @title = list_title
+    @title = list_title.upcase
 	@items = Array.new
   end
   
   # Creates a new Item and adds it to the array of Items
-  def add_item(new_item)
-    item = Item.new(new_item)
+  def add_item(description, due_date)
+    item = Item.new(description, due_date)
     @items.push(item)
   end
   
@@ -26,41 +26,68 @@ class ToDoList
   end
 
   def change_list_title(new_list_title)
-    @title = new_list_title
+    @title = new_list_title.upcase
   end
 
   # Prints the title with a border
   def print_title
-    display_width = 56
+    display_width = 60
     puts "=" * display_width
 	puts ("#{@title}:").center(display_width)
 	puts "=" * display_width
   end
   
+  def print_column_headers
+    display_width = 60
+	column1 = "No."
+	column2 = "Description"
+	column3 = "Due"
+	column4 = "Completed"
+	printf "%s %-30s %-15s %s\n", column1, column2, column3, column4
+	puts "-" * display_width
+  end
+  
   # Prints all the current Items in the ToDoList.
   def print
     print_title
+	print_column_headers
 	count = 0
     @items.each do |item|
       count += 1
-    printf "%s. %-35s Completed?: %s\n", count, item.description, item.completion_status
+      item.print(count)
     end
 	puts
   end
 end
 
 class Item
-  attr_reader :description, :completion_status
+  attr_reader :description, :completion_status, :due_date
 
   # Initialize item with a description and marked as
   # not complete  
-  def initialize(item_description)
+  def initialize(item_description, due_date)
     @description = item_description
+	@due_date = due_date
     @completion_status = false
   end
 
+  def complete?
+    if completion_status then "( X )" else "(   )" end
+  end
+  
   # Changes the completion_status of an Item to true.  
   def mark_complete
     @completion_status = true
+  end
+  
+  def format_date(date)
+    year = date.year
+	month = date.month
+	day = date.day
+	"#{month}/#{day}/#{year}"
+  end
+    
+  def print(count)
+    printf "%2s%s %-30s %-19s %s\n", count, ".", description, format_date(due_date), complete?
   end
 end
--- udacitask/udacitask.rb
+++ udacitask/udacitask.rb
@@ -4,10 +4,10 @@ require_relative 'todolist.rb'
 to_do_list = ToDoList.new("Coit's To Do List")
 
 # Add four new items
-to_do_list.add_item("Organize Closet")
-to_do_list.add_item("Replace Burnt Out Light Bulbs")
-to_do_list.add_item("Buy a Couch")
-to_do_list.add_item("Fix Leaky Faucet")
+to_do_list.add_item("Organize Closet", Time.new(2016,7,1))
+to_do_list.add_item("Replace Light Bulbs", Time.new(2016,6,26))
+to_do_list.add_item("Buy a New Couch", Time.new(2016,7,3))
+to_do_list.add_item("Fix Leaky Faucet", Time.new(2016,6,26))
 
 # Print the list
 to_do_list.print
